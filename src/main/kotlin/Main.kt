9f6efbf90d4328119ed66983e940d3842da6f273
San Andres <cool.snow@mail.ru>
2023-06-01 21:59:57 +0300

    extention functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,19 @@
 import spaceships.SpaceShip
+import java.io.File
 
 fun main(args: Array<String>) {
-    val ship1 = SpaceShip("ship1", 500)
-    ship1.switchToWarpMode()
-    ship1.runSystemDiagnostic()
+    val file = File("text.txt")
+    file.writeToFileAndSend()
+    //val ship1 = SpaceShip("ship1", 500)
+    //ship1.switchToWarpMode()
+    //ship1.runSystemDiagnostic()
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
 }
+fun File.writeToFileAndSend() {
+    val message = "some message"
+    createNewFile()
+    writeText(message)
+    println("message: \"$message\" added to file $name and send ")
+}
+
